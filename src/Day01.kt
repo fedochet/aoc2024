7c885a64b2a69b9a7911edbe6b8572849b168d5f
Roman Golyshev <anstreth@gmail.com>
2024-12-01 19:09:30 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,40 @@
+import kotlin.math.abs
+
 fun main() {
+    fun parseData(input: List<String>): Pair<List<Int>, List<Int>> {
+        val twoNumbersRegex = Regex("(\\d+)\\s+(\\d+)")
+
+        return input
+            .map { line ->
+                val matchResult = twoNumbersRegex.find(line) ?: error("Cannot parse input: $line")
+                val (first, second) = matchResult.destructured
+
+                first.toInt() to second.toInt()
+            }
+            .unzip()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = parseData(input)
+
+        val leftSorted = left.sorted()
+        val rightSorted = right.sorted()
+
+        return leftSorted.zip(rightSorted) { a, b -> abs(a - b) }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val (left, right) = parseData(input)
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        val rightCountMap = right.groupingBy { it }.eachCount().withDefault { 0 }
+
+        return left.sumOf { it * rightCountMap.getValue(it) }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
